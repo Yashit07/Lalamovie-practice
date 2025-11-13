2cf8ed487e51d6a4c573ea843e966c5605c10844
Yashit07 <yashitchawla07@icloud.com>
2025-11-14 03:00:10 +0530

    Just a practice project kinda app

--- Lalamovie/ContentView.swift
+++ Lalamovie/ContentView.swift
@@ -1,32 +1,35 @@
-//
-//  ContentView.swift
-//  Lalamovie
-//
-//  Created by Yashit Chawla on 04/11/25.
-//
-
 import SwiftUI
 
+enum AppTabs {
+    case home, upcoming, downloads, search
+}
+ 
 struct ContentView: View {
+    @State var selectedTab: AppTabs = .home
+    
     var body: some View {
-        TabView {
-            Tab(Constants.homeString, systemImage: Constants.homeIconString) {
+        TabView(selection: $selectedTab) {
+            Tab(Constants.homeString, systemImage: Constants.homeIconString, value: .home) {
                 HomeView()
             }
             
-            Tab(Constants.upcomingString, systemImage: Constants.UpcomingIconString) {
+            Tab(Constants.upcomingString, systemImage: Constants.UpcomingIconString, value: .upcoming) {
                 UpcomingView()
             }
             
-            Tab(Constants.searchString, systemImage: Constants.SearchIconString) {
-                SearchView()
-            }
-            
-            Tab(Constants.downloadsString, systemImage: Constants.DownloadsIconString) {
+            Tab(Constants.downloadsString, systemImage: Constants.DownloadsIconString, value: .downloads) {
                 DownloadView()
             }
+            
+            //            Tab(Constants.searchString, systemImage: Constants.SearchIconString) {
+            //                SearchView()
+            //            }
+            
+            Tab(value: .search, role: .search) {
+                SearchView()
+            }
         }
-        .tint(Color("primaryc")) // Tab bar tint color
+        .tint(Color("primaryc"))
     }
 }
 
